15e1c2697f2a101c916d43e48539ff60d14b6501
CodingwithJess <thejessiebess@gmail.com>
2020-08-02 11:43:11 -0600

    updated orm all function

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,8 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burger
-(
+CREATE TABLE burger(
 	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
+	name varchar (255) NO NULL,
 	PRIMARY KEY (id)
 );
--- models/burger.js
+++ models/burger.js
@@ -2,24 +2,24 @@
 const orm = ("../config/orm.js");
 
 const burger = {
-  all: (cb) => {
-    orm.all("burger", (res) => {
+  all: function(cb){
+    orm.all("burger", function(res){
       cb(res);
     });
   },
   // variables columns and values are arrays
-  create: (cols, vals, cb) => {
-    orm.create("burger", cols, vals, (res) => {
+  create: function(cols, vals, cb){
+    orm.create("burger", cols, vals, function(res){
       cb(res);
     });
   },
-  update: (objColVals, condition, cd) => {
-    orm.update ("burger", objColVals, condition, (res) =>{
+  update: function(objColVals, condition, cd){
+    orm.update ("burger", objColVals, condition, function(res){
       cb(res);
     });
   },
-  delete: (condition, cb) => {
-    orm.delete("burger", condition, (res) =>{
+  delete: function(condition, cb){
+    orm.delete("burger", condition,function(res){
       cb(res);
     });
   }
