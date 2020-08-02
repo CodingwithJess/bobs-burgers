4b2c425afd08af8ccf0137ee071e1aebe6cce145
CodingwithJess <thejessiebess@gmail.com>
2020-08-02 11:03:03 -0600

    updated controller.js and server.js files. Also added schema and seed info from mySQL

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (name) VALUES ("She's a super leek burger");
+INSERT INTO burger (name) VALUES ("If looks could kale burger");
+INSERT INTO burger (name) VALUES ("The final kraut down burger");
