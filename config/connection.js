8be388cfcec1517d73452e073e9a39ee29649cea
CodingwithJess <thejessiebess@gmail.com>
2020-08-03 20:56:50 -0600

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,18 @@
 const mysql = require ("mysql");
 
-const connection = mysql.createConnection ({
-  host: "localhost",
-  port: 3306,
-  user:"root",
-  password: "5419#Mason!",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection= mysql.createConnection(process.env.JAWSDB_URL); 
+} else {
+  connection = mysql.createConnection ({
+    host: "localhost",
+    port: 3306,
+    user:"root",
+    password: "5419#Mason!",
+    database: "burgers_db"
+  });
+}
 
 // making connection
-connection.connect ((err) => {
-  if (err) {
-    console.error(`Error connecting ${err.stack}`);
-    return;
-  }
-  console.log(`Connected as ID ${connection.threadId}`);
-});
+connection.connect();
 
 module.exports = connection;
