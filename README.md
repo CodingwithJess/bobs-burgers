82b77b5efe5c93110d97744243d1dcc0bebbf1e8
CodingwithJess <thejessiebess@gmail.com>
2020-08-03 21:54:44 -0600

    updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# bobs-burgers
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+
+  # Bob's Burgers
+
+  ## Description 
+  Bob's Burgers is burger application that allows a user to add burgers to the menu, consume the burgers, and delete them. When a burger has been eaten, it moves categories on the page.
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions) 
+  
+  ## Installation
+  Run in browser.
+
+  ## Usage 
+  To eat a burger, click on the 'NOMNOMNOM' button. To delete, click on the red X. To add a burger, fill out the input field under 'Add a burger' and click 'Put er on the grill'
+  ## License
+  This site is covered under MIT
+  
+  ## Contributing
+  Please conact me on GitHub to contribute.
+
+  ## Tests
+  n/a
+  
+  ## Questions?
+  https://github.com/CodingwithJess
+  Email me here: jess@codingwithjess.com
+
+  
--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -1,14 +1,14 @@
-var express = require("express");
+const express = require("express");
 
-var router = express.Router();
+const router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger");
+const burger = require("../models/burger");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
+router.get("/",(req, res) => {
   burger.all(function(data) {
-    var hbsObject = {
+    const hbsObject = {
       burger: data
     };
     console.log(hbsObject);
@@ -16,7 +16,7 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/burgers", function(req, res) {
+router.post("/api/burgers",(req, res) => {
   burger.create([
     "name", "eaten"
   ], [
@@ -27,8 +27,8 @@ router.post("/api/burgers", function(req, res) {
   });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+router.put("/api/burgers/:id",(req, res) => {
+  const condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
@@ -44,8 +44,8 @@ router.put("/api/burgers/:id", function(req, res) {
   });
 });
 
-router.delete("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+router.delete("/api/burgers/:id",(req, res) => {
+  const condition = "id = " + req.params.id;
 
   burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
